c672f711901ea3d21cef7468a95453ece519163e
yzsever <yzsever@163.com>
2020-12-27 21:11:25 +0800

    更新文件名

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,6 +8,6 @@ var store = [{
         "title": "基于Docker搭建Mysql主从复制环境",
         "excerpt":"一、阿里云和Docker搭建主从服务器 1、阿里云环境为centos，首先安装docker ~# yum install docker-ce-3:19.03.13-3.el8 -y 2、然后拉取docker镜像，这里使用5.7版本的mysql： ~# docker pull mysql:5.7 3、使用此镜像启动容器，这里需要分别启动主从两个容器 Master(主)： ~# docker run -p 3309:3306 --name primary -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 Slave(从)： ~# docker run -p 3308:3306 --name secondary -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 primary对外映射的端口是3309，secondary对外映射的端口是3308。使用docker ps命令查看正在运行的容器： ~# docker ps CONTAINER ID IMAGE COMMAND CREATED STATUS PORTS NAMES...","categories": ["Mysql"],
         "tags": [],
-        "url": "https://yzsever.github.io/mysql/Mysql%E4%B8%BB%E4%BB%8E%E5%A4%8D%E5%88%B6%E6%90%AD%E5%BB%BA/",
+        "url": "https://yzsever.github.io/mysql/mysql-replica/",
         "teaser": null
       }]
